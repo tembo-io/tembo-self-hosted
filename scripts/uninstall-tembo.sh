322f57b487d618850f4c28d860797a6c3c343fd0
Adarsh Shah <github@shahadarsh.com>
2025-03-04 12:32:07 -0500

    fixes vim values.yaml

--- scripts/uninstall-tembo.sh
+++ scripts/uninstall-tembo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-kubectl get coredb -n tembo --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete coredb -n tembo
+kubectl get coredb -n tembo-system --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete coredb -n tembo-system
 
-kubectl label namespace tembo tembo-pod-init.tembo.io/watch-
+kubectl label namespace tembo-system tembo-pod-init.tembo.io/watch-
 
-helm uninstall tembo -n tembo
+helm uninstall tembo -n tembo-system
 
-kubectl get certificate -n tembo --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete certificate -n tembo
+kubectl get certificate -n tembo-system --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete certificate -n tembo-system
 
-kubectl get secret -n tembo --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete secret -n tembo
+kubectl get secret -n tembo-system --no-headers -o custom-columns=":metadata.name" | xargs kubectl delete secret -n tembo-system
